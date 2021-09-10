312240f4939e6bf5e892672c7b78da51211b0409
JonhParra <jonathan.parra@corebiz.ag>
2021-09-10 11:59:25 -0500

    Update/subiendo cambios de neotel al salvadorqa

--- react/components/Chat/index.js
+++ react/components/Chat/index.js
@@ -1,38 +1,40 @@
-import React, { useState } from "react";
+import React from "react";
+import {Helmet} from "react-helmet";
+
 
 import './global.css';
 
 const Chat = () => {
-  const [bubble, setBubble] = useState(true);
 
-  const handleClick = (e) => {
-    MyG2C.API.chatAsk("Gracias por contactarte al chat de Almacenes Siman con gusto te podemos ayudar, ¿Con quién tenemos el gusto?", 10);
-    /* setBubble(!bubble);
-    let image = e.target.src
-    if (image.includes('bubb')) {
-      MyG2C.API.chatAsk("Solicitud de ayuda.", 10);
-      setbox(true)
-    } else if (image.includes('open')) {
-      console.log('cerrar');
-      setbox(false)
-    } */
-  }
+  const handleClick = (e) => startNeoWebChat()
 
   return (
+    <div>
+        <div>
+            <Helmet>
+                <link rel='stylesheet' href='http://35.196.4.210/CHAT/css/webchat.css' type='text/css' />
 
-    <div className="float">
-      <span className="img-btn" onClick={handleClick}></span>
+                <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>  
+                <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js" ></script>
+                <script src='https://35.196.4.210/chat/js/webchat.js'></script>
+                <script type='text/javascript' language='javascript'>
+                    {`
+                    $(document).ready(function () {
+                        console.log('entrando')
+                        let externalKey = "";
+                        initWebChat("https","35.196.4.210", "CHAT", "3Hlser5a8Z21YCaDH2Ph49WcEWrLNKhZ1TFBu8gWjJTzd/PFTjEF06ddrdS7vBEzpBWtzvYICeN6mC8yN2lCkCyHNs/N58dZAbHlxW2G8pOBhRTStyIWSAzIxM4SOeGzo2PWnXAh3PKG0ct3eczIVinxuc5OHDrjhbouC+Oo3Hi5OtfWF3v/Ng==", "CHAT SALVADOR", "#C71616", "448", "401", "3", externalKey);
+                        console.log('dentro de ')
+                    });
+                    `}
+                </script>
+            </Helmet>
+        </div>
+      <div className="float">
+        <span className="img-btn" onClick={handleClick}></span>
+      </div>
+
     </div>
   );
 };
-/* <>
-     {box && <div className="box">Chat</div>}
-     <div className="float">
-       <div>
-         {bubble ? <img src="https://siman.vtexassets.com/arquivos/chat-bubb.png" width="30%" onClick={handleClick} />
-           : <img src="https://siman.vtexassets.com/arquivos/chat-open.png" width="30%" onClick={handleClick} />
-         }
-       </div>
-     </div>
-   </> */
+
 export default Chat;
