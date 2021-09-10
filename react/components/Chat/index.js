662c365c2921874d91253458d6f61a7239a0b716
JonhParra <jonathan.parra@corebiz.ag>
2021-09-10 13:05:23 -0500

    Update/Creando chat neotel para costarica

--- react/components/Chat/index.js
+++ react/components/Chat/index.js
@@ -22,7 +22,7 @@ const Chat = () => {
                     $(document).ready(function () {
                         console.log('entrando')
                         let externalKey = "";
-                        initWebChat("http","35.196.4.210", "CHAT", "3Hlser5a8Z21YCaDH2Ph49WcEWrLNKhZ1TFBu8gWjJTzd/PFTjEF06ddrdS7vBEzpBWtzvYICePUnWbOGDCFTKQ8zNyteuBSmHpI6CmdOaaC63T9m0LI4bWw1xHLJUNBKsEIL+MUP/Uf/aUZsAyNPNNuRQlhIRRFCFduK70t8hbCjtp19EXY/g==", "CHAT NICARAGUA", "#C71616", "448", "401", "3", externalKey);
+                        initWebChat("http","35.196.4.210", "CHAT", "3Hlser5a8Z21YCaDH2Ph49WcEWrLNKhZ1TFBu8gWjJTzd/PFTjEF06ddrdS7vBEzpBWtzvYICeNv5fekUXThV15ifaZgtTpne7ZdjYMGXqBRRBJDaIa9de2FGH1L4voJ2iDpzPLSju1aPkXOJGzyPeqwcfjGj+1yU5iHvH4GOZNdqR+zyLBs5g==", "CHAT COSTA RICA", "#C71616", "448", "401", "3", externalKey);
                         console.log('dentro de la funcion')
                     });
                     `}
