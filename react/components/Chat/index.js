9a5a710e3660a135ea6db1158ccc08c58c1ce0f6
JonhParra <jonathan.parra@corebiz.ag>
2021-09-13 10:30:06 -0500

    Update/agregando bugs

--- react/components/Chat/index.js
+++ react/components/Chat/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import {Helmet} from "react-helmet";
 
-
 import './global.css';
 
 const Chat = () => {
 
-  const handleClick = (e) => startNeoWebChat()
+  const handleClick = (e) => {
+    startNeoWebChat()
+    const enLinea = '<div class="span-foco"></div><div class="en-linea">En linea </div>'
+    document.querySelector('.WEBCHATTITLE').insertAdjacentHTML('afterend',enLinea)
+  }
 
   return (
     <div>
@@ -20,10 +23,10 @@ const Chat = () => {
                 <script type='text/javascript' language='javascript'>
                     {`
                     $(document).ready(function () {
-                        console.log('entrando')
+                        console.log('entrando nuevo')
                         let externalKey = "";
-                        initWebChat("http","35.196.4.210", "CHAT", "3Hlser5a8Z21YCaDH2Ph49WcEWrLNKhZ1TFBu8gWjJTzd/PFTjEF06ddrdS7vBEzpBWtzvYICePUnWbOGDCFTKQ8zNyteuBSmHpI6CmdOaaC63T9m0LI4bWw1xHLJUNBKsEIL+MUP/Uf/aUZsAyNPNNuRQlhIRRFCFduK70t8hbCjtp19EXY/g==", "CHAT NICARAGUA", "#C71616", "448", "401", "3", externalKey);
-                        console.log('dentro de la funcion')
+                        initWebChat("https","35.196.4.210", "CHAT", "3Hlser5a8Z21YCaDH2Ph49WcEWrLNKhZ1TFBu8gWjJTzd/PFTjEF06ddrdS7vBEzpBWtzvYICePUnWbOGDCFTKQ8zNyteuBSmHpI6CmdOaaC63T9m0LI4bWw1xHLJUNBKsEIL+MUP/Uf/aUZsAyNPNNuRQlhIRRFCFduK70t8hbCjtp19EXY/g==", "¿Necesitas ayuda?", "#C71616", "448", "401", "3", externalKey);
+                        console.log('dentro de la funcion nuevo')
                     });
                     `}
                 </script>
