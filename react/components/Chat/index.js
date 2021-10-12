3cadfc19d5ed2de2018bc765f15ff22c5165c83d
AnallelyJB <anallely.jimenez@corebiz.ag>
2021-10-12 15:24:54 -0500

    Update index.js
    Web chat -> Cambio de dominio en loas URL y se dejó de llamar al archivo webchat.css

--- react/components/Chat/index.js
+++ react/components/Chat/index.js
@@ -1,47 +1,40 @@
 import React from "react";
 import {Helmet} from "react-helmet";
 
-
 import './global.css';
 
 const Chat = () => {
-
-  const handleClick = (e) => {
-    startNeoWebChat()
-    if(document.querySelector('.span-foco') === null){
-      const enLinea = '<div class="span-foco"></div><div class="en-linea">En linea </div>'
-      document.querySelector('.WEBCHATTITLE').insertAdjacentHTML('afterend',enLinea)
+    
+    const handleClick = (e) => {
+        startNeoWebChat()
+        if(!document.querySelector('.en-linea')){
+            const enLinea = '<div class="span-foco"></div><div class="en-linea">En linea </div>'
+            document.querySelector('.WEBCHATTITLE').insertAdjacentHTML('afterend', enLinea)
+        }
     }
-  }
-
-
-  return (
-    <div>
+    
+    return (
         <div>
-            <Helmet>
-                <link rel='stylesheet' href='http://35.196.4.210/CHAT/css/webchat.css' type='text/css' />
-
-                <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>  
-                <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js" ></script>
-                <script src='https://35.196.4.210/chat/js/webchat.js'></script>
-                <script type='text/javascript' language='javascript'>
+            <div>
+                <Helmet>
+                    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>  
+                    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js" ></script>
+                    <script src='https://neotel.hthconsultores.com/chat/js/webchat.js'></script>
+                    <script type='text/javascript' language='javascript'>
                     {`
-                    $(document).ready(function () {
-                        console.log('entrando')
-                        let externalKey = "";
-                        initWebChat("https","35.196.4.210", "CHAT", "3Hlser5a8Z21YCaDH2Ph49WcEWrLNKhZ1TFBu8gWjJTzd/PFTjEF06ddrdS7vBEzpBWtzvYICeN6mC8yN2lCkCyHNs/N58dZAbHlxW2G8pOBhRTStyIWSAzIxM4SOeGzo2PWnXAh3PKG0ct3eczIVinxuc5OHDrjhbouC+Oo3Hi5OtfWF3v/Ng==", "¿Necesitas ayuda?", "#C71616", "448", "401", "3", externalKey);
-                        console.log('dentro de ')
-                    });
+                        $(document).ready(function () {
+                            let externalKey = "";
+                            initWebChat("https","neotel.hthconsultores.com", "chat", "3Hlser5a8Z21YCaDH2Ph49WcEWrLNKhZ1TFBu8gWjJTzd/PFTjEF06ddrdS7vBEzpBWtzvYICeN6mC8yN2lCkCyHNs/N58dZAbHlxW2G8pOBhRTStyIWSAzIxM4SOeGzo2PWnXAh3PKG0ct3eczIVinxuc5OHDrjhbouC+Oo3Hi5OtfWF3v/Ng==", "¿Necesitas ayuda?", "#C71616", "448", "401", "3", externalKey);
+                        });
                     `}
-                </script>
-            </Helmet>
+                    </script>
+                </Helmet>
+            </div>
+            <div className="float">
+                <span className="img-btn" onClick={handleClick}></span>
+            </div>
         </div>
-      <div className="float">
-        <span className="img-btn" onClick={handleClick}></span>
-      </div>
-
-    </div>
-  );
+    );
 };
-
+    
 export default Chat;
